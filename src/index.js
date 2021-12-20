b0ac8537730504ab96170f95b61b478062f5b38f
Lara <laraellenh@gmail.com>
2021-12-20 11:44:14 -0500

    complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+const todoForm = document.getElementById("create-task-form" );
+// elements are just objects, pass submit through, then the handleSubmit function will be invoked
+todoForm.addEventListener('submit', handleSubmit);
+
+  
 });
+function handleSubmit(event) {
+  event.preventDefault();
+console.log(event.target.taskDescription.value)
+
+const taskList = document.getElementById('tasks ');
+const task = document.createElement('li');
+task.textContent = event.target.taskDescription.value;
+taskList.append(task);
+}
