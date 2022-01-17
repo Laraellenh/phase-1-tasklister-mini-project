f8b931a0f31a9117f3fe78b441c82a09f43238f5
Lara <laraellenh@gmail.com>
2022-01-17 16:06:51 -0500

    did the things, DOM things

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
+
+// load dom elements first, grab form, set submit event
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-const todoForm = document.getElementById("create-task-form" );
-// elements are just objects, pass submit through, then the handleSubmit function will be invoked
-todoForm.addEventListener('submit', handleSubmit);
+    let form = document.querySelector('form')
+    form.addEventListener('submit', (e) => {
+        // prevent auto refresh
+        e.preventDefault();
+        //   form is parent to inputs, we can chain on attribute input id newtastkdescription
+        buildTodo(e.target.newtaskdescription.value)
+        form.reset()
+    })
+})
+// function to handle form, this function will be invoked above for the submit event, giving it a place to put the input
+function buildTodo(todo) {
+    // make a dom element P tag to append the input there
+    let p = document.createElement('p')
+    // make a button with "x" click event that will .remove when clicked
+    let button = document.createElement('button')
+    button.addEventListener('click', handleDelete)
+    button.textContent = "x"
+    p.textContent = todo
+    p.appendChild(button)
+    document.getElementById("list").appendChild(p)
+}
 
-  
-});
-function handleSubmit(event) {
-  event.preventDefault();
-console.log(event.target.taskDescription.value)
-
-const taskList = document.getElementById('tasks ');
-const task = document.createElement('li');
-task.textContent = event.target.taskDescription.value;
-taskList.append(task);
+// the target of the event is the button
+function handleDelete(e) {
+    e.target.parentNode.remove()
 }
